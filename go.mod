a97ad320fb4df2043330fdb22172309471fa435a
Dimitrii <sexykdi@gmail.com>
2020-07-14 10:30:51 +0300

    initial

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/LdDl/osm2ch
+
+go 1.14
+
+require (
+	github.com/LdDl/ch v1.3.4
+	github.com/paulmach/go.geojson v1.4.0
+	github.com/paulmach/osm v0.1.0
+)
---
+++ osm_configuration.go
@@ -0,0 +1,17 @@
+package osm2ch
+
+// OsmConfiguration Allows to filter ways by certain tags from OSM data
+type OsmConfiguration struct {
+	EntityName string // Currrently we support 'highway' only
+	Tags       []string
+}
+
+// CheckTag Checks if incoming tag is represented in configuration
+func (cfg *OsmConfiguration) CheckTag(tag string) bool {
+	for i := range cfg.Tags {
+		if cfg.Tags[i] == tag {
+			return true
+		}
+	}
+	return false
+}
