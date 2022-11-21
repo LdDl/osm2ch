a7b958702d1f00517c0c47214e3feceaf22903a3
Dimitrii <sexykdi@gmail.com>
2022-11-21 14:56:52 +0300

    main arguments

---
+++ parser.go
@@ -0,0 +1,150 @@
+package osm2ch
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Parser struct {
+	filename         string
+	networkTypes     []string
+	linkTypes        []string
+	preparePOI       bool
+	poiSamplingRatio float64
+	strictMode       bool
+	offset           string
+	minNodes         int
+	combine          bool
+	defaultLanes     map[string]interface{}
+	defaultSpeed     map[string]float64
+	defaultCapacity  map[string]float64
+	startNodeID      int
+	startLinkID      int
+}
+
+func (parser *Parser) String() string {
+	return fmt.Sprintf(`
+Network parser parameters:
+	filename: %s
+	network_types: %s
+	link_types: %s
+	prepare POI?: %t
+	POI_sampling_ratio: %f
+	strict_mode enabled?: %t
+	offset: %s
+	min_nodes: %d
+	combine: %t
+	default_lanes: %v
+	default_speed: %v
+	default_capacity: %v
+	start_node_id: %d
+	start_link_id: %d
+	`,
+		parser.filename,
+		strings.Join(parser.networkTypes, ","),
+		strings.Join(parser.linkTypes, ","),
+		parser.preparePOI,
+		parser.poiSamplingRatio,
+		parser.strictMode,
+		parser.offset,
+		parser.minNodes,
+		parser.combine,
+		parser.defaultLanes,
+		parser.defaultSpeed,
+		parser.defaultCapacity,
+		parser.startNodeID,
+		parser.startLinkID,
+	)
+}
+
+func NewParser(options ...func(*Parser)) *Parser {
+	parser := &Parser{}
+	for _, option := range options {
+		option(parser)
+	}
+	return parser
+}
+
+func WithFilename(filename string) func(*Parser) {
+	return func(parser *Parser) {
+		parser.filename = filename
+	}
+}
+
+func WithNetworkTypes(networkTypes []string) func(*Parser) {
+	return func(parser *Parser) {
+		parser.networkTypes = networkTypes
+	}
+}
+
+func WithLinkTypes(linkTypes []string) func(*Parser) {
+	return func(parser *Parser) {
+		parser.linkTypes = linkTypes
+	}
+}
+
+func WithPreparePOI(preparePOI bool) func(*Parser) {
+	return func(parser *Parser) {
+		parser.preparePOI = preparePOI
+	}
+}
+
+func WithPOISamplingRatio(poiSamplingRatio float64) func(*Parser) {
+	return func(parser *Parser) {
+		parser.poiSamplingRatio = poiSamplingRatio
+	}
+}
+
+func WithStrictMode(strictMode bool) func(*Parser) {
+	return func(parser *Parser) {
+		parser.strictMode = strictMode
+	}
+}
+
+func WithOffset(offset string) func(*Parser) {
+	return func(parser *Parser) {
+		parser.offset = offset
+	}
+}
+
+func WithMinNodes(minNodes int) func(*Parser) {
+	return func(parser *Parser) {
+		parser.minNodes = minNodes
+	}
+}
+
+func WithCombine(combine bool) func(*Parser) {
+	return func(parser *Parser) {
+		parser.combine = combine
+	}
+}
+
+func WithDefaultLanes(defaultLanes map[string]interface{}) func(*Parser) {
+	return func(parser *Parser) {
+		parser.defaultLanes = defaultLanes
+	}
+}
+
+func WithDefaultSpeed(defaultSpeed map[string]float64) func(*Parser) {
+	return func(parser *Parser) {
+		parser.defaultSpeed = defaultSpeed
+	}
+}
+
+func WithDefaultCapacity(defaultCapacity map[string]float64) func(*Parser) {
+	return func(parser *Parser) {
+		parser.defaultCapacity = defaultCapacity
+	}
+}
+
+func WithStartNodeID(startNodeID int) func(*Parser) {
+	return func(parser *Parser) {
+		parser.startNodeID = startNodeID
+	}
+}
+
+func WithStartLinkID(startLinkID int) func(*Parser) {
+	return func(parser *Parser) {
+		parser.startLinkID = startLinkID
+	}
+}
