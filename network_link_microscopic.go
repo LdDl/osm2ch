acb06b9af4a95f4b1c3a632addea26d7d70bfb01
Dimitrii <sexykdi@gmail.com>
2023-02-01 19:35:33 +0300

    prepare node/link for micro

--- network_link_microscopic.go
+++ network_link_microscopic.go
@@ -1,5 +1,31 @@
 package osm2ch
 
+import (
+	"github.com/paulmach/orb"
+)
+
+type MicroscopicLinkType uint16
+
+const (
+	LINK_FORWARD = MicroscopicLinkType(1)
+	LINK_LANE_CHANGE
+)
+
 type NetworkLinkMicroscopic struct {
+	geom          orb.LineString
+	geomEuclidean orb.LineString
+
 	ID NetworkLinkID
+
+	sourceNodeID NetworkNodeID // Corresponds to ID of microscopic node (not to meso or macro or OSM)
+	targetNodeID NetworkNodeID // Corresponds to ID of microscopic node (not to meso or macro or OSM)
+
+	mesoLinkID NetworkLinkID
+
+	microLinkType         MicroscopicLinkType
+	allowedAgentTypes     []AgentType
+	firstMovement         bool
+	movementCompositeType MovementCompositeType // Inherited from movement of parent mesoscopic link (if firstMovement = true)
+	controlType           ControlType           // Inherited from mesoscipoc link
+	laneID                int                   // Inherited from source node
 }
--- network_node_microscopic.go
+++ network_node_microscopic.go
@@ -1,5 +1,15 @@
 package osm2ch
 
+import "github.com/paulmach/orb"
+
 type NetworkNodeMicroscopic struct {
+	geom orb.Point
+
 	ID NetworkNodeID
+
+	laneID                     int
+	isLinkUpstreamTargetNode   bool
+	isLinkDownstreamTargetNode bool
+	zoneID                     NetworkNodeID // Should be inherited from the macroscopic node which is target (isLinkUpstreamTargetNode = true) or source (isLinkDownstreamTargetNode = true) node of parent mesoscopic link
+	boundaryType               BoundaryType  // Should be evaluated from macroscopic node which is target (isLinkUpstreamTargetNode = true) or source (isLinkDownstreamTargetNode = true) node of parent mesoscopic link
 }
